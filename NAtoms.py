4cb778d8df91f5675a908587cc70150b62be7be8
Tommy <tommy.myrvik@fys.uio.no>
2018-07-15 22:05:08 +0200

    Lagt til noe ny kode og noen oppgaver

--- NAtoms.py
+++ NAtoms.py
@@ -9,7 +9,7 @@ in this file will be imported.
 """
 
 def LJ_pot(r, epsilon=1, sigma=1):
-    """Function for computing the potential of the Lennard-Jones Potential.
+    """Function for computing the potential of the Lennard-Jones Potential. Cut-off at r = 3.0.
 
     Args:
         r: float or array of floats with distances between atoms.
@@ -19,8 +19,11 @@ def LJ_pot(r, epsilon=1, sigma=1):
     Returns:
         The computed potential between the atoms given as input.
     """
-
-    return 4*epsilon*((sigma/r)**12 - (sigma/r)**6)
+    if r < 3.0:
+        U_corr = 4*epsilon*((sigma/3.0)**12 - (sigma/3.0)**6)
+        return 4*epsilon*((sigma/r)**12 - (sigma/r)**6) - U_corr
+    else:
+        return 0
 
 def LJ_force(r, epsilon=1, sigma=1):
     """Function for computing the forces of the Lennard-Jones Potential.
@@ -64,7 +67,8 @@ def plot_LJ():
 
 def simulate(r0, v0, m, T, dt, intg, wrt_file = False):
 
-    """ A function for simulating the 2-atom model with an integration method of choice.
+    """ A function for simulating the N-atom model with an integration method of choice. Stores all
+    positions and velocities at every time steps in arrays. May run into memory issues.
 
     Args:
         r0: Array with the initial condition for positions.
@@ -148,9 +152,106 @@ def simulate(r0, v0, m, T, dt, intg, wrt_file = False):
 
     return r, v, t
 
+def simulate_sparse(r0, v0, m, T, dt, intg, filename):
+
+    """ A function for simulating the N-atom model with an integration method of choice. Same as simulate(),
+    but doesn't store positions and velocities in arrays, but writes them to file instead.
+
+    Args:
+        r0: Array with the initial condition for positions.
+        v0: Array with the initial condition for velocities.
+        m: mass of the atoms
+        T: The end time where the integration will stop.
+        dt: Size of the time step.
+        intg: String telling what method to use; either 'Euler', 'EulerCromer' og 'VelVerlet'.
+        filename: Name of the file the data will be written to. Needs an extension like ".xyz".
+
+    returns:
+        t: Array for the discrete time points.
+    """
+
+    nt = int(T/dt) + 1   # Number of timesteps
+    t = np.linspace(0, T, nt)
+
+    N = int(r0.shape[0]/3)
+
+    r = r0
+    v = v0
+
+    infile = open(filename, 'w')
+    infile.write("%i\n\n" %(N))
+
+    for j in range(N):
+        infile.write(str(r[3*j: 3*j +3])[1:-1] + " " + str(v[3*j : 3*j +3])[1:-1] + "\n")
+
+
+    # Time integration
+    for i in range(nt-1):
+
+        infile.write("%i\n\n" %(N))
+
+        if intg == 'VelVerlet' and i != 0:
+            a = a_next
+
+        else:
+
+            F = np.zeros(3*N)
+
+            for j in range(N):
+                for k in range(j+1, N):
+                    d_vec = r[3*j: 3*j+3] - r[3*k: 3*k+3]
+                    d = np.sqrt(np.sum((d_vec)**2))
+
+                    F_jk = LJ_force(d)*d_vec/d
+                    F[3*j: 3*j+3] += F_jk
+                    F[3*k: 3*k+3] -= F_jk
+
+            a = F/m
+
+        if intg == 'Euler':
+            v_next = v + a*dt
+            r_next = r + v*dt
+
+        elif intg == 'EulerCromer':
+            v_next = v + a*dt
+            r_next = r + v_next*dt
+
+        elif intg == 'VelVerlet':
+            r_next = r + v*dt + 0.5*a*dt**2
+
+            F_next = np.zeros(3*N)
+
+            for j in range(N):
+                for k in range(j+1, N):
+                    d_vec = r_next[3*j: 3*j+3] - r_next[3*k: 3*k+3]
+                    d = np.sqrt(np.sum((d_vec)**2))
+
+                    F_jk = LJ_force(d)*d_vec/d
+                    F_next[3*j: 3*j+3] += F_jk
+                    F_next[3*k: 3*k+3] -= F_jk
+
+            a_next = F_next/m
+
+            v_next = v + 0.5*(a + a_next)*dt
+
+        else:
+            print('Error: The variable intg must either be "Euler", "EulerCromer" or "VelVerlet".\nExiting...')
+            sys.exit(0)
+
+        for j in range(N):
+            infile.write(str(r_next[3*j : 3*j +3])[1:-1] + " " + str(v_next[3*j : 3*j +3])[1:-1]+ "\n")
+
+        r = r_next
+        v = v_next
+
+    infile.close()
+
+    return t
+
 
 def Energy(r, v, t, m, plot = True):
-    """ Calculate both the kinetic and potential energies of the 2-atom model
+    """ Calculate both the kinetic and potential energies of the N-atom model from the stored
+    position and velocity arrays generated in simulate().
 
     Args:
         r: Array for positions.
@@ -178,6 +279,74 @@ def Energy(r, v, t, m, plot = True):
 
                 d = np.sqrt(np.sum((r[i, 3*j: 3*j +3] - r[i, 3*k: 3*k +3])**2))
                 PotEng[i] += LJ_pot(d)
+
+    if plot:
+        fig, (ax1, ax2) = plt.subplots(2, 1)
+        ax1.set_title("Kinetic and Potential energy of the " + str(N) + "-atom system")
+        ax1.plot(t, KinEng)
+        #ax1.set_ylim([-1, 3])
+        ax1.set_xlabel(r"$t$")
+        ax1.set_ylabel(r"$K(t)$")
+        ax1.grid(True)
+        ax2.plot(t, PotEng)
+        #ax2.set_ylim([-3, 3])
+        ax2.set_xlabel(r"$t$")
+        ax2.set_ylabel(r"$U(t)$")
+        ax2.grid(True)
+        plt.show()
+
+    return KinEng, PotEng
+
+def Energy_sparse(N, t, m, filename, plot = True):
+    """ Calculate both the kinetic and potential energies of the N-atom model, reads positions
+    and velocities from file generated by simulate_sparse().
+
+    Args:
+        N: Number of atoms
+        t: Array for the discrete time points.
+        m: Mass of the atoms.
+        filename: Name of the file to read data from. Assumes that the file has the form:
+            ----------------------------------
+            <No. atoms>
+
+            <r_1x r_1y r_1z v_1x v_1y v_1z>   # Positions and velocities of atom 1
+            ...
+            <r_Nx r_Ny r_Nz v_Nx v_Ny v_Nz>   # Positions and velocities of atom N
+            ----------------------------------
+            This snippet repeats for all time steps.
+        plot: Boolean expression, whether to plot the results or not, 'True' as default.
+
+    Returns:
+        KinEng: Array for the kinetic energy at each timestep.
+        PotEng: Array for the potential energy at each timestep.
+    """
+    from itertools import islice
+
+
+    nt = t.shape[0]
+    KinEng = np.zeros(nt)
+    PotEng = np.zeros(nt)
+
+    #infile = open(filename, 'r')
+    with open(filename, 'r') as infile:
+        for i in range(nt):
+            r = np.empty(0)
+            line_gen = islice(infile, 2, N+2)
+            j = 0
+            for line in line_gen:
+                tmp = np.array(line.split(), dtype=np.float)
+                r = np.concatenate((r, tmp[:3]))
+                v = tmp[3:]
+
+                KinEng[i] += 0.5*m*np.sum(v**2)
+
+                for k in range(j):
+                    d_vec = r[3*j : 3*j +3] - r[3*k : 3*k + 3]
+                    d = np.sqrt(np.sum(d_vec**2))
+
+                    PotEng[i] += LJ_pot(d)
+
+                j += 1
 
     if plot:
         fig, (ax1, ax2) = plt.subplots(2, 1)
--- NAtoms_tasks.py
+++ NAtoms_tasks.py
@@ -12,7 +12,7 @@ def test():
     m = 1
 
     #r0 = np.random.randint(1, 5, size=3*N)
-    r0 = np.random.uniform(0.0, 25.0, size = 3*N)
+    r0 = np.random.uniform(0.0, 50.0, size = 3*N)
     #r0 = np.array([-2, 0, 0, 2, 0, 0, 0, -2, 0, 0, 2, 0])
 
     v0 = np.zeros(3*N)
@@ -26,6 +26,16 @@ def test():
     print("Tot. time: %.3f s" % (t_stop - t_start))
     print("Time pr iter: %.3f s" % ((t_stop - t_start)/(nt-1)))
 
+    t_start = time.clock()
+
+    t = simulate_sparse(r0, v0, m, T, dt, 'VelVerlet', "in2.xyz")
+
+    t_stop = time.clock()
+
+    print("Tot. time: %.3f s" % (t_stop - t_start))
+    print("Time pr iter: %.3f s" % ((t_stop - t_start)/(nt-1)))
+
+    """
     KinEng, PotEng = Energy(r, v, t, m)
 
     plt.plot(t, (KinEng + PotEng))
@@ -34,22 +44,61 @@ def test():
     plt.ylabel(r'$E(t)$')
     plt.grid(True)
     plt.show()
+    """
 
 def test2():
 
-    N = 4
+    N = 512
     m = 1
     T = 10
-    dt = 0.01
+    dt = 0.05
+    nt = int(T/dt) + 1
+    filename = "in2.xyz"
 
     #r0 = np.array([-1.5, 0, 0, 1.5, 0, 0, 0, -1.5, 0, 0, 1.5, 0])
-    r0 = np.array([-2, 0, 0, 2, 0, 0, 0, -2, 0, 0, 2, 0])
-    #r0 = np.array([0, 0, 0, -1.5, 0, 0])
+    #r0 = np.array([-2, 0, 0, 2, 0, 0, 0, -2, 0, 0, 2, 0])
+    r0 = np.random.uniform(0.0, 100.0, size = 3*N)
     v0 = np.zeros(3*N)
 
-    r, v,t = simulate(r0, v0, m, T, dt, 'VelVerlet', wrt_file=True)
+    t_start = time.clock()
 
-    KinEng, PotEng = Energy(r, v, t, m)
+    t = simulate_sparse(r0, v0, m, T, dt, 'VelVerlet', filename)
+
+    t_stop = time.clock()
+    print("Tot. time sparse simulate: %.3f s" % (t_stop - t_start))
+    print("Time pr iter: %.3f s" % ((t_stop - t_start)/(nt-1)))
+
+    t_start = time.clock()
+
+    KinEng, PotEng = Energy_sparse(N, t, m, filename, plot=True)
+
+    t_stop = time.clock()
+    print("Tot. time energy sparse: %.3f s\n " % (t_stop - t_start))
+
+
+
+    plt.plot(t, (KinEng + PotEng))
+    plt.title("Total energy as function of time")
+    plt.xlabel(r'$t$')
+    plt.ylabel(r'$E(t)$')
+    plt.grid(True)
+    plt.show()
+
+    t_start = time.clock()
+
+    r, v, t = simulate(r0, v0, m, T, dt, 'VelVerlet', filename)
+
+    t_stop = time.clock()
+    print("Tot. time simulate: %.3f s" % (t_stop - t_start))
+    print("Time pr iter: %.3f s" % ((t_stop - t_start)/(nt-1)))
+
+    t_start = time.clock()
+
+    KinEng, PotEng = Energy(r, v, t, m, plot=True)
+
+    t_stop = time.clock()
+    print("Tot. time energy: %.3f s" % (t_stop - t_start))
+
 
     plt.plot(t, (KinEng + PotEng))
     plt.title("Total energy as function of time")
@@ -59,4 +108,4 @@ def test2():
     plt.show()
 
 if __name__ == "__main__":
-    test()
+    test2()
